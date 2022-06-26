c4d781af6d7b1bee308a375122b2f01fdae9c39c
OrangeB0lt <667@holbertonschool.com>
2022-06-25 22:42:59 -0400

    version 1

--- js/jquery.particleground.js
+++ js/jquery.particleground.js
@@ -377,17 +377,17 @@
     maxSpeedX: 0.7,
     minSpeedY: 0.1,
     maxSpeedY: 0.7,
-    directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
-    directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
+    directionX: 'down', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
+    directionY: 'up', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
     density: 10000, // How many particles will be generated: one particle every n pixels
     dotColor: '#666666',
     lineColor: '#666666',
     particleRadius: 7, // Dot size
     lineWidth: 1,
     curvedLines: false,
-    proximity: 100, // How close two dots need to be before they join
+    proximity: 92, // How close two dots need to be before they join
     parallax: true,
-    parallaxMultiplier: 5, // The lower the number, the more extreme the parallax effect
+    parallaxMultiplier: 3, // The lower the number, the more extreme the parallax effect
     onInit: function() {},
     onDestroy: function() {}
   };
